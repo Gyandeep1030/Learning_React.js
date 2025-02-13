2991bab5560c0064fbcd1ee6c35e95817fe5d9ce
Gyandeep <Gyandeep1030@gmail.com>
2025-02-13 16:32:25 +0530

    first commit

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -218,7 +218,9 @@ let MyApp = () => {
 
   */
 
+// ? Hooks Examples--
 
+/*
   let MyApp = () => {
 
     let [i , setCount] = useState(0);
@@ -237,6 +239,173 @@ let MyApp = () => {
             <button onClick={() => {setCount(i+1)}}>Increment</button>
             <button onClick={() => {setCount(0)}}>Reset</button>
             <button onClick={() => {setCount(i-1)}}>Decrement</button>
+        </center>
+    )
+
+
+
+  }
+  export default MyApp;
+
+*/
+
+
+
+/*
+
+let MyApp = () => {
+
+    let [i , setCount_i] = useState(0);
+    let [j , setCount_j] = useState(0);
+
+    return(
+        <center>
+            <h1>Testing on Hooks</h1>
+            <br/>
+            <h2>Count : {i}</h2>
+            <br/>
+            <button onClick={() => {setCount_i(10)}}>Increse by 10 </button>
+            <button onClick={() => {setCount_i(20)}}>Increse by 20</button>
+            <br/>
+            <br/>
+            
+            <button onClick={() => {setCount_i(i+1)}}>Increment</button>
+            <button onClick={() => {setCount_i(0)}}>Reset</button>
+            <button onClick={() => {setCount_i(i-1)}}>Decrement</button>
+
+
+            <h2>Count : {j}</h2>
+            <br/>
+            <button onClick={() => {setCount_j(10)}}>Increse by 10 </button>
+            <button onClick={() => {setCount_j(20)}}>Increse by 20</button>
+            <br/>
+            <br/>
+            
+            <button onClick={() => {setCount_j(j+1)}}>Increment</button>
+            <button onClick={() => {setCount_j(0)}}>Reset</button>
+            <button onClick={() => {setCount_j(j-1)}}>Decrement</button>
+        </center>
+    )
+
+
+
+  }
+  export default MyApp;
+
+
+  */
+
+
+
+
+
+
+  /*
+   let MyApp = () => {
+
+    let [i , setCount] = useState("Gyandeep");
+
+    return(
+        <center>
+            <h1>Testing on Hooks</h1>
+            <br/>
+            <h2>Name : {i}</h2>
+            <br/>
+            <button onClick={() => {setCount("Gyandeep")}}>First name </button>
+            <button onClick={() => {setCount("Gyandeep Kumar")}}>Full Name</button>
+            <br/>
+            <br/>
+        </center>
+    )
+
+
+
+  }
+  export default MyApp;
+
+  */
+
+
+
+  /*
+  let MyApp = () => {
+
+    let [i , setCount] = useState(false);
+
+    return(
+        <center>
+            <h1>Testing on Hooks</h1>
+            <br/>
+            <h2>Count : {i ? "Male" : "Female"}</h2>
+            <br/>
+            <button onClick={() => {setCount(true)}}>Male </button>
+            <button onClick={() => {setCount(false)}}>Female</button>
+        </center>
+    )
+
+
+
+  }
+  export default MyApp;
+
+  */
+
+
+/*
+
+  let MyApp = () => {
+
+    let [i , setSkill] = useState(["Py" , "Dejango" , "Numpay"]);
+
+    return(
+        <center>
+            <h1>Testing on Hooks</h1>
+            <br/>
+            <h2>Skills : {i.map((val , ind) => {return <li key={ind}>{val}</li>})}</h2>
+            <br/>
+            <button onClick={() => {setSkill(["Py" , "Dejango" , "Numpay"])}}>Python </button>
+            <button onClick={() => {setSkill(["springBoot" , "TheamLeaf" , "Hibernate"])}}>JAVA</button>
+            <button onClick={() => {setSkill(["MongoDB" , "Express.js", "React.js "])}}>MEARN</button>
+        </center>
+    )
+
+
+
+  }
+  export default MyApp;
+
+
+  */
+
+
+  let MyApp = () => {
+
+    let student1 = {name :"Gyandeep"  , roll : 1  , age: 20  };
+    let student2 = {name : "Tanmay" , roll : 2 , age:25  };
+    let student3 = {name :"Shivani"  , roll : 21 , age: 30 };
+    let student4 = {name :"Rahul"  , roll : 3 , age:21  };
+
+
+
+
+    let [Stu , set] = useState(student1);
+
+    return(
+        <center>
+            <h1>Testing on Hooks</h1>
+            <br/>
+            <br/>
+            <button onClick={() => set(student1)}>Fist Student </button>
+            <button onClick={() => set(student2)}>Second Student</button>
+            <button onClick={() => set(student3)}>Third Student</button>
+
+            <br/>
+            <br/>
+            <h2>Name : {Stu.name}</h2>
+            <h2>Name : {Stu.age}</h2>
+            <h2>Name : {Stu.roll}</h2>
+
+
         </center>
     )
 
