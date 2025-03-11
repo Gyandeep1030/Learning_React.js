8ba3894ef7e9f54f58b5840d1aada83944291bca
Gyandeep <Gyandeep1030@gmail.com>
2025-03-11 22:44:19 +0530

    3/11/25

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1050,7 +1050,7 @@ export default app;
 
 */
 
-
+/*
 import React from 'react'
 import ChildComponent from './ChildComponents';
 class app extends React.Component {
@@ -1092,3 +1092,103 @@ class app extends React.Component {
 }
 
 export default app;
+*/
+
+
+
+/* -------------------Note-----------------
+    ? Step 1:
+        npm install axios
+        npm i axios
+
+    ? Step 2:
+        import axios from 'axios'
+    
+    ? Step 3:
+        axios.get(url)
+        axios.post(url, data)
+        axios.put(url, data)
+        axios.patch(url, data)
+        axios.delete(url)
+
+*/
+
+import React from 'react'
+import axios from 'axios'
+
+function App() {
+
+    let [data , setData] = React.useState([]);
+
+    React.useEffect(() => {
+        let getData = async () => {
+            let { data } = await axios.get('https://fakestoreapi.com/products')
+            setData(data);
+        };
+        getData();
+    }, [])
+
+    
+
+
+    return ( <>
+        <h1>Products</h1>
+        <ul>
+
+        <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+            {data.map((val , ind) => {
+                return <li key={ind}>
+                    <img src={val.image} alt="img"  />
+                    <li>
+                        {val.title}
+                    </li>
+                        {val.price}
+                    </li>
+            })}
+        </ul>
+    
+    </> );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="bg-white">
+  <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+    <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div class="group relative">
+        <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80">
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Basic Tee
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Black</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">$35</p>
+        </div>
+      </div>
+
+      <!-- More products... -->
+    </div>
+  </div>
+</div>
